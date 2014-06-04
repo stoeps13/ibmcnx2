385679dcb42318ffa052cf71ff4f8776aafa7db1
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-06-04 14:20:09 +0200

    New tests

---
+++ ibmcnx/config/DataSources.py
@@ -0,0 +1,70 @@
+######
+#  Check ExId (GUID) by Email through JDBC
+#
+#  Author:        Christoph Stoettner
+#  Mail:          christoph.stoettner@stoeps.de
+#  Documentation: http://scripting101.stoeps.de
+#
+#  Version:       2.0
+#  Date:          2014-06-04
+#
+#  License:       Apache 2.0
+#
+#  Check ExId of a User in all Connections Applications
+
+import ConfigParser
+import ibmcnx.functions
+
+configParser = ConfigParser.ConfigParser()
+configFilePath = r'ibmcnx/ibmcnx.properties'
+configParser.read(configFilePath)
+
+perf = {'activities':{'minConnections':configParser.get('Tuning','opnact.min'), 'maxConnections':configParser.get('Tuning','opnact.max')},
+         'blogs':{'minConnections':configParser.get('Tuning','blogs.min'), 'maxConnections':configParser.get('Tuning','blogs.max')},
+         'communities':{'minConnections':configParser.get('Tuning','sncomm.min'), 'maxConnections':configParser.get('Tuning','sncomm.max')},
+         'dogear':{'minConnections':configParser.get('Tuning','dogear.min'), 'maxConnections':configParser.get('Tuning','dogear.max')},
+         'files':{'minConnections':configParser.get('Tuning','files.min'), 'maxConnections':configParser.get('Tuning','files.max')},
+         'FNOSDS':{'minConnections':configParser.get('Tuning','fnosds.min'), 'maxConnections':configParser.get('Tuning','fnosds.max')},
+         'FNOSDSXA':{'minConnections':configParser.get('Tuning','fnosdsxa.min'), 'maxConnections':configParser.get('Tuning','fnosdsxa.max')},
+         'forum':{'minConnections':configParser.get('Tuning','forum.min'), 'maxConnections':configParser.get('Tuning','forum.max')},
+         'homepage':{'minConnections':configParser.get('Tuning','homepage.min'), 'maxConnections':configParser.get('Tuning','homepage.max')},
+         'metrics':{'minConnections':configParser.get('Tuning','metrics.min'), 'maxConnections':configParser.get('Tuning','metrics.max')},
+         'mobile':{'minConnections':configParser.get('Tuning','mobile.min'), 'maxConnections':configParser.get('Tuning','mobile.max')},
+         'news':{'minConnections':configParser.get('Tuning','news.min'), 'maxConnections':configParser.get('Tuning','news.max')},
+         'profiles':{'minConnections':configParser.get('Tuning','profiles.min'), 'maxConnections':configParser.get('Tuning','profiles.max')},
+         'search':{'minConnections':configParser.get('Tuning','search.min'), 'maxConnections':configParser.get('Tuning','search.max')},
+         'wikis':{'minConnections':configParser.get('Tuning','wikis.min'), 'maxConnections':configParser.get('Tuning','wikis.max')}}
+
+statementCacheSize = configParser.get('Tuning','statementCache')
+
+print 'DataSource Parameters are set to: '
+print 'Database \t statementCacheSize \t minConnections \t maxConnections'
+for db in perf.keys():
+    print db.upper(),
+    if len( db ) < 7:
+        print '\t',
+    print '\t\t' + str( statementCacheSize ),
+    print '\t\t\t' + str( perf[db]['minConnections'] ),
+    print '\t\t\t' + str( perf[db]['maxConnections'] )
+
+answer = raw_input( 'Are you sure, that your parameters should be overwritten? (Yes|No) ' )
+allowed_answer = ['yes', 'y', 'ja', 'j']
+
+if answer.lower() in allowed_answer:
+    for db in perf.keys():    # Looping through databases
+        print 'Change DataSource parameters for: %s' % db.upper()
+        try:
+            t1 = ibmcnx.functions.getDSId( db )
+            print '\t\tstatementCacheSize: \t' + str( statementCacheSize )
+            print '\t\tminConnections: \t' + str( perf[db]['minConnections'] )
+            print '\t\tmaxConnections: \t' + str( perf[db]['maxConnections'] )
+            AdminConfig.modify( t1, '[[statementCacheSize "' + str( statementCacheSize ) + '"]]' )
+            AdminConfig.modify( t1, '[[connectionPool [[minConnections "' + str( perf[db]['minConnections'] ) + '"][maxConnections "' + str( perf[db]['maxConnections'] ) + '"]]]]' )
+            print '\tParameter for %s successfully set!\n' % db.upper()
+        except:
+            print '\tError can\'t set Performance parameter for' + db.upper() + '!'
+    print "Saving changes!"
+    AdminConfig.save()
+
+else:
+    print '\t\tNothing changed! '
--- ibmcnx/member/CheckExId.py
+++ ibmcnx/member/CheckExId.py
@@ -1,69 +1,109 @@
-######
-#  Check ExId (GUID) by Email through JDBC
+# To run this script with Windows or SLES, you have to modify setupCmdLine
 #
-#  Author:        Christoph Stoettner
-#  Mail:          christoph.stoettner@stoeps.de
-#  Documentation: http://scripting101.stoeps.de
+# setupCmdLine.bat|sh
+# add on line 40: "SET WAS_USER_SCRIPT=d:\ibm\wasuserscript.cmd"
 #
-#  Version:       2.0
-#  Date:          2014-06-04
+# Create d:\ibm\wasuserscript.cmd:
+# "SET WAS_EXT_DIRS=%WAS_EXT_DIRS%;c:\ibm\sqllib\java"
 #
-#  License:       Apache 2.0
-#
-#  Check ExId of a User in all Connections Applications
 
+import os
+import sys
+from java.util import Properties
 import ConfigParser
-import ibmcnx.functions
 
+# Only load commands if not initialized directly (call from menu)
+if __name__ != "__main__":
+    execfile("ibmcnx/loadCnxApps.py")
+
+import com.ibm.db2.jcc.DB2Driver as Driver
+
+# Get configuration from properties file
 configParser = ConfigParser.ConfigParser()
 configFilePath = r'ibmcnx/ibmcnx.properties'
 configParser.read(configFilePath)
 
-perf = {'activities':{'minConnections':configParser.get('Tuning','opnact.min'), 'maxConnections':configParser.get('Tuning','opnact.max')},
-         'blogs':{'minConnections':configParser.get('Tuning','blogs.min'), 'maxConnections':configParser.get('Tuning','blogs.max')},
-         'communities':{'minConnections':configParser.get('Tuning','sncomm.min'), 'maxConnections':configParser.get('Tuning','sncomm.max')},
-         'dogear':{'minConnections':configParser.get('Tuning','dogear.min'), 'maxConnections':configParser.get('Tuning','dogear.max')},
-         'files':{'minConnections':configParser.get('Tuning','files.min'), 'maxConnections':configParser.get('Tuning','files.max')},
-         'FNOSDS':{'minConnections':configParser.get('Tuning','fnosds.min'), 'maxConnections':configParser.get('Tuning','fnosds.max')},
-         'FNOSDSXA':{'minConnections':configParser.get('Tuning','fnosdsxa.min'), 'maxConnections':configParser.get('Tuning','fnosdsxa.max')},
-         'forum':{'minConnections':configParser.get('Tuning','forum.min'), 'maxConnections':configParser.get('Tuning','forum.max')},
-         'homepage':{'minConnections':configParser.get('Tuning','homepage.min'), 'maxConnections':configParser.get('Tuning','homepage.max')},
-         'metrics':{'minConnections':configParser.get('Tuning','metrics.min'), 'maxConnections':configParser.get('Tuning','metrics.max')},
-         'mobile':{'minConnections':configParser.get('Tuning','mobile.min'), 'maxConnections':configParser.get('Tuning','mobile.max')},
-         'news':{'minConnections':configParser.get('Tuning','news.min'), 'maxConnections':configParser.get('Tuning','news.max')},
-         'profiles':{'minConnections':configParser.get('Tuning','profiles.min'), 'maxConnections':configParser.get('Tuning','profiles.max')},
-         'search':{'minConnections':configParser.get('Tuning','search.min'), 'maxConnections':configParser.get('Tuning','search.max')},
-         'wikis':{'minConnections':configParser.get('Tuning','wikis.min'), 'maxConnections':configParser.get('Tuning','wikis.max')}}
+# Change User and Password
+props = Properties()
+props.put( 'user', configParser.get('Database','dbUser') )
+props.put( 'password', configParser.get('Database','dbPassword') )
 
-statementCacheSize = configParser.get('Tuning','statementCache')
+jdbcPath = 'jdbc:db2://' + configParser.get('Database','dbHost') + ':' + configParser.get('Database','dbPort') + '/' + configParser.get('Database','dbName')
 
-print 'DataSource Parameters are set to: '
-print 'Database \t statementCacheSize \t minConnections \t maxConnections'
-for db in perf.keys():
-    print db.upper(),
-    if len( db ) < 7:
-        print '\t',
-    print '\t\t' + str( statementCacheSize ),
-    print '\t\t\t' + str( perf[db]['minConnections'] ),
-    print '\t\t\t' + str( perf[db]['maxConnections'] )
+# Change Hostname, Port and maybe DB Name
+conn = Driver().connect( jdbcPath, props )
 
-answer = raw_input( 'Are you sure, that your parameters should be overwritten? (Yes|No) ' )
-allowed_answer = ['yes', 'y', 'ja', 'j']
+stmt = conn.createStatement()
 
-if answer.lower() in allowed_answer:
-    for db in perf.keys():    # Looping through databases
-        print 'Change DataSource parameters for: %s' % db.upper()
-        try:
-            t1 = ibmcnx.functions.getDSId( db )
-            print '\t\tstatementCacheSize: \t' + str( statementCacheSize )
-            print '\t\tminConnections: \t' + str( perf[db]['minConnections'] )
-            print '\t\tmaxConnections: \t' + str( perf[db]['maxConnections'] )
-            AdminConfig.modify( t1, '[[statementCacheSize "' + str( statementCacheSize ) + '"]]' )
-            AdminConfig.modify( t1, '[[connectionPool [[minConnections "' + str( perf[db]['minConnections'] ) + '"][maxConnections "' + str( perf[db]['maxConnections'] ) + '"]]]]' )
-            print 'Parameter for %s successfully set!' % db.upper()
-        except:
-            print '\tError can\'t set Performance parameter for' + db.upper() + '!'
-    AdminConfig.save()
+email = raw_input( "Mail address of profile you want to check: " ).lower()
 
-else:
-    print '\t\tNothing changed! '
+sql = 'select PROF_UID_LOWER,PROF_MAIL_LOWER,PROF_GUID,PROF_MAIL from empinst.employee where PROF_MAIL_LOWER = \'' + email + '\' order by PROF_UID_LOWER'
+rs = stmt.executeQuery( sql )
+
+employeeList = []
+while ( rs.next() ):
+    row = {}
+    row['PROF_UID_LOWER'] = rs.getString( 1 )
+    row['PROF_MAIL_LOWER'] = rs.getString( 2 )
+    row['PROF_GUID'] = rs.getString( 3 )
+    row['PROF_MAIL'] = rs.getString( 4 )
+    employeeList.append( row )
+
+rs.close()
+stmt.close()
+conn.close()
+
+# print the result
+for e in employeeList:
+    # print e['PROF_UID_LOWER'] + "\t\t" + e['PROF_MAIL_LOWER'] + "\t\t" + e['PROF_GUID']
+    # print e['PROF_MAIL']
+    print "Profiles:\t\t\t " + e['PROF_GUID']
+
+LOGIN = e['PROF_MAIL']
+try:
+   print "Activities:\t\t\t",
+   ActivitiesMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "Blogs:\t\t\t\t",
+   BlogsMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "Communities:\t\t\t",
+   CommunitiesMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "Dogear:\t\t\t\t",
+   DogearMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "Files:\t\t\t\t",
+   FilesMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "Forums:\t\t\t\t",
+   ForumsMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "News, Search, Homepage:\t\t",
+   NewsMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "Wikis:\t\t\t\t",
+   WikisMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'

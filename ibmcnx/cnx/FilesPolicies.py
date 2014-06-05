bb17493a0c092ad3787fbf29e05f2aa522349969
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-06-05 14:47:48 +0200

    Some Menu work

--- ibmcnx/cnx/FilesPolicies.py
+++ ibmcnx/cnx/FilesPolicies.py
@@ -19,7 +19,7 @@ import java
 #if __name__ == "__main__":
 #    execfile("filesAdmin.py")
 
-execfile("filesAdmin.py", bAskForNodeFiles, bAskForNodeFiles)
+reload("filesAdmin.py")
 
 def printPolicies( policies ):
     state = ''

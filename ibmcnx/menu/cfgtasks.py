ff36973f14f63d2a529571033aeff58288b210da
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-06-05 11:18:17 +0200

    Some Menu work

--- ibmcnx/menu/cfgtasks.py
+++ ibmcnx/menu/cfgtasks.py
@@ -16,11 +16,11 @@ import ibmcnx.functions
 import ibmcnx.menu.MenuClass
 
 # Load all jython commands, when they are not loaded
-try:
-    NewsActivityStreamService.listApplicationRegistrations()
-except NameError:
-    print "Connections Commands not loaded! Load now: "
-    execfile("ibmcnx/loadCnxApps.py")
+#try:
+#    NewsActivityStreamService.listApplicationRegistrations()
+#except NameError:
+#    print "Connections Commands not loaded! Load now: "
+#    execfile("ibmcnx/loadCnxApps.py")
 
 cfg = ibmcnx.menu.MenuClass.cnxMenu()
 cfg.AddItem( "Configure DataSources (cfgDataSource.py)", ibmcnx.functions.cfgDataSource )

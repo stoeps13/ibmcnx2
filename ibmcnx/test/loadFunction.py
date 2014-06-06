067c80c647b7811d1853fde41a6ebfe0ea9beada
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-06-06 12:04:59 +0200

    Customize scripts to work with menu

--- ibmcnx/test/loadFunction.py
+++ ibmcnx/test/loadFunction.py
@@ -1,3 +1,9 @@
+
+import sys
+from java.lang import String
+from java.util import HashSet
+from java.util import HashMap
+import java
 globdict = globals()
 def loadFilesService():
     global globdict

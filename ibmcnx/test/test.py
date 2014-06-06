f1099201de2e19b40653409ee17a309cc1f70440
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-06-06 12:04:18 +0200

    Customize scripts to work with menu

--- ibmcnx/test/test.py
+++ ibmcnx/test/test.py
@@ -1,4 +1,9 @@
 import ibmcnx.test.loadFunction
+import sys
+from java.lang import String
+from java.util import HashSet
+from java.util import HashMap
+import java
 
 ibmcnx.test.loadFunction.loadFilesService()
 

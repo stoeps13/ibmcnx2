efce0e17a193ca59e472d707a0315517d64cb73f
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-06-10 15:13:50 +0200

    Read all jvm settings
    Task-Url: http://github.com/stoeps13/ibmcnx2/issues/issue/3

---
+++ ibmcnx/doc/JVMSettings.py
@@ -0,0 +1,34 @@
+######
+#  Print JVM Settings of all appservers, dmgr and nodeagent
+#
+#  Author:        Christoph Stoettner
+#  Mail:          christoph.stoettner@stoeps.de
+#  Documentation: http://scripting101.stoeps.de
+#
+#  Version:       2.0
+#  Date:          2014-06-09
+#
+#  License:       Apache 2.0
+#
+
+#  Get a list of all servers in WAS cell (dmgr, nodeagents, AppServer, webserver)
+servers = AdminTask.listServers().splitlines()
+#  Get a list of all webservers
+webservers = AdminTask.listServers( '[-serverType WEB_SERVER]' ).splitlines()
+
+#  Remove webserver from servers list
+for webserver in webservers:
+    servers.remove( webserver )
+
+jvmSettingsList = [classpath, bootClasspath, verboseModeClass, verboseModeGarbageCollection, verboseModeJNI, initialHeapSize, maximumHeapSize, runHProf, hprofArguments, debugMode, debugArgs, genericJvmArguments, executableJarFileName, disableJIT, osName, internalClassAccessMode ]
+
+for server in servers:
+    jvm = AdminConfig.list( 'JavaVirtualMachine', server )
+    srv = server.split( '/' )
+    cell = srv[1]
+    node = srv[3]
+    servername = srv[5].split( '|' )[0]
+    print "%s - %s - %s" % ( cell, node, servername )
+    for item in jvmSettingsList:
+        print "\t" + item + ': \t' + AdminConfig.showAttribute( jvm, item )
+        print ' '

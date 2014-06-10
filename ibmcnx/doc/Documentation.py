5f69110a4a343a8ab6d3cc6b6efc6ca145897d94
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-06-10 15:59:02 +0200

    4: Create script to save documentation to a file
    Task-Url: http://github.com/stoeps13/ibmcnx2/issues/issue/4

---
+++ ibmcnx/doc/Documentation.py
@@ -0,0 +1,35 @@
+######
+#  Create a file (html or markdown) with the output of
+#      - JVMHeap
+#      - LogFiles
+#      - Ports
+#      - Variables
+#
+#  Author:        Christoph Stoettner
+#  Mail:          christoph.stoettner@stoeps.de
+#  Documentation: http://scripting101.stoeps.de
+#
+#  Version:       2.0
+#  Date:          2014-06-08
+#
+#  License:       Apache 2.0
+#
+
+# TODO: Create a menu for file selection
+
+import ibmcnx.filehandle
+import sys
+
+sys.stdout = open("/tmp/documentation.txt", "w")
+
+print '# JVM Settings of all AppServers:'
+execfile( 'ibmcnx/doc/JVMSettings.py' )
+
+print '# Used Ports:'
+execfile( 'ibmcnx/doc/Ports.py' )
+
+print '# LogFile Settgins:'
+execfile( 'ibmcnx/doc/LogFiles.py' )
+
+print '# WebSphere Variables'
+execfile( 'ibmcnx/doc/Variables.py' )

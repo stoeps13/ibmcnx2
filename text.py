e5bb825e5cea355c0679b0a1074cfb846f25f637
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-06-10 15:51:46 +0200

    2: Read all jvm settings
    Task-Url: http://github.com/stoeps13/ibmcnx2/issues/issue/2

--- text.py
+++ text.py
@@ -2,7 +2,7 @@ import ibmcnx.filehandle
 
 emp1 = ibmcnx.filehandle.Ibmcnxfile()
 
-emp1.writeToFile("Test")
-emp1.writeToFile("Test2")
-emp1.writeToFile("Test3")
+emp1.writeToFile( execfile('ibmcnx/doc/JVMSettings.py' ) )
+#emp1.writeToFile("Test2")
+#emp1.writeToFile("Test3")
 emp1.closeFile()

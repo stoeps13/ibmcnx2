fe4f2fa1c64d40a15c49cc4183a59c912574fdff
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-06-10 17:17:00 +0200

    4: Create script to save documentation to a file
    Task-Url: http://github.com/stoeps13/ibmcnx2/issues/issue/4

--- ibmcnx/doc/DataSources.py
+++ ibmcnx/doc/DataSources.py
@@ -14,6 +14,7 @@
 
 import ibmcnx.functions
 
+print AdminControl.getCell()
 cell = "'/Cell:" + AdminControl.getCell() + "/'"
 cellid = AdminConfig.getid( cell )
 dbs = AdminConfig.list( 'DataSource', str(cellid) )

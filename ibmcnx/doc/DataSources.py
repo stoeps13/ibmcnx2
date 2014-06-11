e9ce2c9ce5cd95926b3c48cb56735f9d1f57ca40
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-06-11 08:15:57 +0200

    8: Create documentation of DataSource Settings
    Task-Url: http://github.com/stoeps13/ibmcnx2/issues/issue/8

--- ibmcnx/doc/DataSources.py
+++ ibmcnx/doc/DataSources.py
@@ -15,12 +15,10 @@
 import ibmcnx.functions
 
 dbs = AdminConfig.list('DataSource', AdminConfig.getid('/Cell:cnxwas1Cell01/'))
-# print dbs
-type(dbs)
-dblist = []
+
 for db in dbs:
-    dblist.append(db)
-print dblist
+    print db
+    
 # dbs = dbs.split('(')[0]
 # print dbs
 #  dbs = ['FNOSDS', 'FNGCDDS', 'IBM_FORMS_DATA_SOURCE', 'activities', 'blogs', 'communities', 'dogear', 'files', 'forum', 'homepage', 'metrics', 'mobile', 'news', 'oauth provider', 'profiles', 'search', 'wikis']    # List of all databases to check

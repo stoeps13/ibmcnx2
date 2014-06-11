504045eb346fd8ff3ce968a3140520cff99165cc
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-06-11 08:09:41 +0200

    8: Create documentation of DataSource Settings
    Task-Url: http://github.com/stoeps13/ibmcnx2/issues/issue/8

--- ibmcnx/doc/DataSources.py
+++ ibmcnx/doc/DataSources.py
@@ -14,12 +14,14 @@
 
 import ibmcnx.functions
 
-dbs = ['FNOSDS', 'FNGCDDS', 'IBM_FORMS_DATA_SOURCE', 'activities', 'blogs', 'communities', 'dogear', 'files', 'forum', 'homepage', 'metrics', 'mobile', 'news', 'oauth provider', 'profiles', 'search', 'wikis']    # List of all databases to check
-
-for db in dbs:
-    t1 = ibmcnx.functions.getDSId( db )
-    AdminConfig.show( t1 )
-    print '\n\n'
-    AdminConfig.showall( t1 )
-    AdminConfig.showAttribute(t1,'statementCacheSize' )
-    AdminConfig.showAttribute(t1,'[statementCacheSize]' )
+dbs = AdminConfig.list('DataSource', AdminConfig.getid('/Cell:cnxwas1Cell01/'))
+print dbs
+#  dbs = ['FNOSDS', 'FNGCDDS', 'IBM_FORMS_DATA_SOURCE', 'activities', 'blogs', 'communities', 'dogear', 'files', 'forum', 'homepage', 'metrics', 'mobile', 'news', 'oauth provider', 'profiles', 'search', 'wikis']    # List of all databases to check
+#  
+#  for db in dbs:
+#      t1 = ibmcnx.functions.getDSId( db )
+#      AdminConfig.show( t1 )
+#      print '\n\n'
+#      AdminConfig.showall( t1 )
+#      AdminConfig.showAttribute(t1,'statementCacheSize' )
+#      AdminConfig.showAttribute(t1,'[statementCacheSize]' )

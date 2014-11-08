25712b9c94062f41c50a8611c5b7069bde7e1c8f
stoeps13 <christoph.stoettner@stoeps.de>
2014-11-08 16:46:46 +0100

    Add option to get temppath from properties file

--- ibmcnx/cnx/VersionStamp.py
+++ ibmcnx/cnx/VersionStamp.py
@@ -11,9 +11,15 @@
 #  License:       Apache 2.0
 #
 
+import ibmcnx.functions
+
 print "\nSet Version Stamp in LotusConnections-config.xml to actual Date and Time\n"
 
-path = raw_input( "Path and Folder where config is temporarily stored: " )
+# Check properties if temppath is defined
+if ( ibmcnx.functions.tempPath() == '' ):
+    path = raw_input( "Path and Folder where config is temporarily stored: " )
+else:
+    path = ibmcnx.functions.tempPath()
 
 execfile("connectionsConfig.py")
 LCConfigService.checkOutConfig(path,AdminControl.getCell())

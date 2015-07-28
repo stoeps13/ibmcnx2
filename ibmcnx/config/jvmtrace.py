eb5891a3a099c598fac34b251df7a2506bf7beca
Christoph Stoettner <christoph.stoettner@stoeps.de>
2015-07-28 17:16:50 +0200

    initial version: set jvm trace - not tested, not functional

---
+++ ibmcnx/config/jvmtrace.py
@@ -0,0 +1,22 @@
+######
+#  set jvm trace 
+#
+#  Author:        Christoph Stoettner
+#  Mail:          christoph.stoettner@stoeps.de
+#  Documentation: http://scripting101.stoeps.de
+#
+#  Version:       2.0
+#  Date:          2015-07-28
+#
+#  License:       Apache 2.0
+#
+
+
+ts = AdminControl.queryNames("type=TraceService,*" )
+tss=ts.split()
+print '\n\t JVM Trace Settings of all servers'
+print '\t =================================\n'
+for trace in tss:
+       print '\t ' + str(trace.split(',')[1].split("=")[1])
+       spec = AdminControl.getAttribute( trace, "traceSpecification")
+       print '\t\t ' + spec + '\n'

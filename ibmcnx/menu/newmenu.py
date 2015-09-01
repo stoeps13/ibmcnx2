d9ec0d911cf967edbb6574a36cee312f46fcaa00
Christoph Stoettner <christoph.stoettner@stoeps.de>
2015-09-01 17:40:56 +0200

    Initial version of new menusystem to provide consistent options for Exit and Back.

---
+++ ibmcnx/menu/newmenu.py
@@ -0,0 +1,32 @@
+'''
+  Script to create menues for Community Connections Scripts
+
+  Author:        Christoph Stoettner
+  Mail:          christoph.stoettner@stoeps.de
+  Documentation: http://scripting101.stoeps.de
+
+  Version:       2.1
+  Date:          2015-09-01
+
+  License:       Apache 2.0
+
+'''
+
+def createMenu( menupoints ):
+    '''
+        Function to calculate numbers for all menus
+
+        menupoints ( list of strings )
+    '''
+    for item in menupoints:
+        print item
+
+def main():
+    '''
+        Main function to create the menu
+    '''
+    menu=[ ['Configuration Tasks', 'cfgtasks.py'] ['B', 'Back to Menu', 'func1'], ['X', 'Exit', 'func2'] ]
+
+    createMenu( menu )
+
+main()

e5e3d956d079c44a9162e4ed522a3d3108b9ef19
Christoph Stoettner <christoph.stoettner@stoeps.de>
2015-09-07 15:46:14 +0200

    Typo in function name

--- ibmcnx/menu/checks.py
+++ ibmcnx/menu/checks.py
@@ -18,21 +18,25 @@ import ibmcnx.functions
 import ibmcnx.menu.MenuClass
 
 #  Only load commands if not initialized directly (call from menu)
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    execfile( "ibmcnx/loadCnxApps.py" )
 
 checks = ibmcnx.menu.MenuClass.cnxMenu()
-checks.AddItem( 'Check if all Apps are running (ibmcnx/check/AppStatus.py)', ibmcnx.functions.checkAppStatus )
-checks.AddItem( 'Check Database connections (ibmcnx/check/DataSource.py)', ibmcnx.functions.checkDataSource )
-checks.AddItem( 'Check Webserver (ibmcnx/check/WebSrvStatus.py)', ibmcnx.functions.checkWebserver )
-checks.AddItem( 'Back to Main Menu (cnxmenu.py)', ibmcnx.functions.cnxBackToMainMenu )
-checks.AddItem( "Exit", ibmcnx.functions.bye )
+checks.AddItem('Check if all Apps are running (ibmcnx/check/AppStatus.py)',
+               ibmcnx.functions.checkAppStatus)
+checks.AddItem('Check Database connections (ibmcnx/check/DataSource.py)',
+               ibmcnx.functions.checkDataSource)
+checks.AddItem('Check Webserver (ibmcnx/check/WebSrvStatus.py)',
+               ibmcnx.functions.checkWebServer)
+checks.AddItem('Back to Main Menu (cnxmenu.py)',
+               ibmcnx.functions.cnxBackToMainMenu)
+checks.AddItem("Exit", ibmcnx.functions.bye)
 
 state_checks = 'True'
 menutitle = "IBM Connections Check Tasks"
 while state_checks == 'True':
-    count = len( checks.menuitems )
-    checks.Show( menutitle )
+    count = len(checks.menuitems)
+    checks.Show(menutitle)
 
     ###########################
     #  # Robust error handling ##
@@ -40,16 +44,16 @@ while state_checks == 'True':
     ###########################
     ## Wait for valid input in while...not ###
     is_valid_checks = 0
-    while not is_valid_checks :
-        try :
-                inputstring = '\tEnter your choice [1-' + str( count ) + ']: '
-                n = int ( raw_input( inputstring ) )
+    while not is_valid_checks:
+        try:
+            inputstring = '\tEnter your choice [1-' + str(count) + ']: '
+            n = int(raw_input(inputstring))
 
-                if n <= count and n > 0:
-                    is_valid_checks = 1    #  # set it to 1 to validate input and to terminate the while..not loop
-                else:
-                    print ( "'%s' is not a valid menu option." ) % n
-        except ValueError, e :
-                print ( "'%s' is not a valid integer." % e.args[0].split( ": " )[1] )
+            if n <= count and n > 0:
+                is_valid_checks = 1  # set it to 1 to validate input and to terminate the while..not loop
+            else:
+                print ("'%s' is not a valid menu option.") % n
+        except ValueError, e:
+            print ("'%s' is not a valid integer." % e.args[0].split(": ")[1])
    #  n = input( "your choice> " )
-    checks.Do( n - 1 )
+    checks.Do(n - 1)

e39a2e04905690460774922c2641c65f65223dac
Christoph Stoettner <christoph.stoettner@stoeps.de>
2015-09-14 22:42:24 +0200

    Initial version

---
+++ ibmcnx/doc/ProfRoleID.py
@@ -0,0 +1,13 @@
+'''
+Print all user with ROLE_ID=employee.extended
+
+Author:        Christoph Stoettner
+Mail:          christoph.stoettner@stoeps.de
+Documentation: http://scripting101.stoeps.de
+
+Version:       @@version@@
+Date:          @@date@@
+
+License:       Apache 2.0
+
+'''

5240ab6c02660189194b71ef722b9d24fe132310
Christoph Stoettner <christoph.stoettner@stoeps.de>
2015-09-14 23:07:01 +0200

    Initial version

---
+++ ibmcnx/cnx/ProfilesAddExtRole.py
@@ -0,0 +1,12 @@
+'''
+Add extended role to a user
+
+Author:        Christoph Stoettner
+Mail:          christoph.stoettner@stoeps.de
+Documentation: http://scripting101.stoeps.de
+
+Version:       @@version@@
+Date:          @@date@@
+
+License:       Apache 2.0
+'''

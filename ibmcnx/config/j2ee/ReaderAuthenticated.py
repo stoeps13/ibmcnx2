bc391f90b361282ddf75e33524b2c2186f901958
Christoph Stoettner <christoph.stoettner@stoeps.de>
2024-12-09 16:35:19 +0100

    Update ReaderAuthenticated.py
    Make exception for RichTextEditors

--- ibmcnx/config/j2ee/ReaderAuthenticated.py
+++ ibmcnx/config/j2ee/ReaderAuthenticated.py
@@ -2,15 +2,15 @@
 Set Reader Roles to Restricted - no anonymous access possible
 
 Description:
-Script is tested with HCL Connections 5
+Script is tested with HCL Connections 8
 
 Author: Christoph Stoettner
-Blog: http://www.stoeps.de
+Blog: https://stoeps.de
 E-Mail:
 Documentation: http://scripting101.stoeps.de
 
-Version:       5.0.1
-Date:          09/19/2015
+Version:       5.0.2
+Date:          2024-12-09
 
 License:       Apache 2.0
 
@@ -18,6 +18,7 @@ History:
 20131124  Christoph Stoettner     Update with loop and try/except to handle errors, added group support
 20140324  Christoph Stoettner     Changed all reader roles to "All authenticated", Ajax proxy security is configured with this role!
 20140716  Christoph Stoettner     Set the script to only change reader role
+20241209  Christoph Stoettner     Create exception for RichTextEditors and set Reader to Everyone here (Highlights and Overview load in loop if set to authenticated)
 '''
 
 import ibmcnx.functions
@@ -27,7 +28,11 @@ appsList = apps.splitlines()
 for app in appsList:
     print "Setting Reader Role to Authenticated for %s" % app.upper()
     try:
-        AdminApp.edit(app, '[-MapRolesToUsers [["reader" No Yes "" ""] ]]')
-        ibmcnx.functions.saveChanges()
+        if app == 'RichTextEditors':
+            print("Don't change RTE app")
+            AdminApp.edit(app, '[-MapRolesToUsers [["reader" Yes No "" ""] ]]')
+        else:
+            AdminApp.edit(app, '[-MapRolesToUsers [["reader" No Yes "" ""] ]]')
     except:
         print "No Reader Role in %s" % app.upper()
+ibmcnx.functions.saveChanges()
